922fb05cedaab55a3eee1c33613fbe0c6686d044
Albert Attard <albertattard@gmail.com>
2020-05-21 08:29:46 +0200

    Update App.java

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,7 +1,7 @@
 package demo;
 
 public class App {
-  public static void main( String[] args ) {
+  public static void main( final String[] args ) {
     System.out.println( "a simple app" );
   }
 }
